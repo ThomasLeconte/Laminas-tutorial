1acf0ac8790d0056d5eeb8b1db358502b3350483
ThomasLeconte <thomasleconte05@gmail.com>
2021-10-04 23:01:49 +0200

    Feat: Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Laminas tutorial
+
+Laminas album tutorial done with MySQL database instead of Sqlite.
+
+## Need to know
+
+> - Run `composer install` before launch for the first time this project.  
+> If you get error, execute this command : `composer install --ignore-platform-reqs` (man hack I know)
+
+> - You can run project through command `composer serve` which will start it on http://127.0.0.1:8080/ by default. You can change it in composer.json
+
+> - You can change MYSQL configuration in config/autoload.php. Then you will have to make `composer dump-autoload`. Then, for be sure of applied modifications,
+> you can delete /data/cache/module-config-cache.application.config.cache.php for generate cache with your own configuration.
+
+### Contact me
+
+- Discord: Bast#0207
+- Mail: thomasleconte05@gmail.com
---
+++ module/Album/src/Module.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Album;
+
+use Laminas\Db\ResultSet\ResultSet;
+use Laminas\Db\Adapter\AdapterInterface;
+use Laminas\Db\TableGateway\TableGateway;
+use Laminas\ModuleManager\Feature\ConfigProviderInterface;
+
+class Module implements ConfigProviderInterface
+{
+    public function getConfig()
+    {
+        return include __DIR__ . '/../config/module.config.php';
+    }
+    
+    /**
+     * With this function you define factory to return your futures database mapped objects
+     * 
+     * Error : "Unable to resolve service "Album\AdapterInterface" -> Import AdapterInterface
+     */
+    public function getServiceConfig()
+    {
+        return [
+            'factories' => [
+                Model\AlbumTable::class => function($container) {
+                    $tableGateway = $container->get(Model\AlbumTableGateway::class);
+                    return new Model\AlbumTable($tableGateway);
+                },
+                Model\AlbumTableGateway::class => function ($container) {
+                    $dbAdapter = $container->get(AdapterInterface::class);
+                    $resultSetPrototype = new ResultSet();
+                    $resultSetPrototype->setArrayObjectPrototype(new Model\Album());
+                    return new TableGateway('album', $dbAdapter, null, $resultSetPrototype);
+                },
+            ],
+        ];
+    }
+
+    /**
+     * Define factory for AlbumController with AlbumTable parameter
+     * 
+     * Error: "Unable to resolve service "Album\Controller\AlbumController" to a factory" -> Remove controllers factories at start of your Album/config/module.config.php
+     */
+    public function getControllerConfig()
+    {
+        return [
+            'factories' => [
+                Controller\AlbumController::class => function($container) {
+                    return new Controller\AlbumController(
+                        $container->get(Model\AlbumTable::class)
+                    );
+                },
+            ],
+        ];
+    }
+}
